34b7835bb6160f17a2a268ff3156243eae10382e
Raghav <raghav.dutta29@gmail.com>
2020-04-14 14:27:17 -0400

    Learning Builder pattern: Part2

---
+++ Design_Patterns/Creational/src/main/java/com/interviewprep/designpattern/builder/LunchOrderDemo.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author raghavdutta
+ * 
+ * @tiApr. 14, 2020
+ */
+package com.interviewprep.designpattern.builder;
+
+/**
+ * Usage of the builder pattern
+ * @author raghavdutta
+ *
+ */
+public class LunchOrderDemo {
+	
+	public static void main(String[] args) {
+		
+		LunchOrder.Builder builder = new LunchOrder.Builder();
+		
+		builder.bread("Wheat").condiments("Lettuce").dressing("Mayo").meat("Turkey");
+		
+		LunchOrder lunchOrder = builder.build();
+		System.out.println(lunchOrder.getBread());
+		System.out.println(lunchOrder.getCondiments());
+		System.out.println(lunchOrder.getDressing());
+		System.out.println(lunchOrder.getMeat());
+		
+		
+	}
+
+}
