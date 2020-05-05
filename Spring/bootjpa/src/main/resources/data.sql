561a0bc03a37e099291e5ed1712d24de9192980a
Raghav <raghav.dutta29@gmail.com>
2020-05-05 00:00:50 -0400

    adding inital sql file to populate data

---
+++ Spring/bootjpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101, 'Raghav', 'Java');
+insert into alien values (102, 'Jack', 'Python');
+insert into alien values (103, 'Erik', 'Swift');
+insert into alien values (104, 'Jane', 'C');
+insert into alien values (105, 'Antoine', 'C++');
