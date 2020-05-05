4b8794b93ec9ac554b83130ac7885ef1ca389b83
Raghav <raghav.dutta29@gmail.com>
2020-05-05 02:11:41 -0400

    adding PUT, DELETE, POST and GET operations

--- Spring/bootjpa/src/main/java/com/raghav/demo/controller/AlienController.java
+++ Spring/bootjpa/src/main/java/com/raghav/demo/controller/AlienController.java
@@ -4,17 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.raghav.demo.dao.AlienRepository;
 import com.raghav.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 
 	@Autowired
@@ -24,11 +26,22 @@ public class AlienController {
 	public String home() {
 		return "home.jsp";
 	}
-
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
+	@DeleteMapping(path="/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "deleted";
+	}
+	@PutMapping(path="/alien",consumes= {"application/json"})
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
 		repo.save(alien);
-		return "home.jsp";
+		return alien;
+	}
+
+	@PostMapping(path="/alien",consumes = {"application/json"})
+	public Alien addAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
 	}
 
 	//	@RequestMapping("/getAlien")
@@ -43,14 +56,12 @@ public class AlienController {
 	//		return mv;
 	//	}
 	//	
-	@RequestMapping("/aliens")
-	@ResponseBody
+	@GetMapping("/aliens")
 	public List<Alien> getAliens() {
 		return repo.findAll();
 	}
 
 	@RequestMapping("/alien/{aid}")
-	@ResponseBody
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 	}
