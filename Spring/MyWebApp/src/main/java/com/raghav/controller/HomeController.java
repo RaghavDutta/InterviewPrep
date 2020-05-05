9c10d3963ff49154beb76dbd58efec0d7f22f638
Raghav <raghav.dutta29@gmail.com>
2020-05-04 22:31:26 -0400

    Using ModelAndView instead of HttpSession

--- Spring/MyWebApp/src/main/java/com/raghav/controller/HomeController.java
+++ Spring/MyWebApp/src/main/java/com/raghav/controller/HomeController.java
@@ -6,16 +6,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping("home")
-	public String home(@RequestParam("name") String myName, HttpSession session)
+	public ModelAndView home(@RequestParam("name") String myName)
 	{
-		System.out.println("Hi "+myName);
-		session.setAttribute("name", myName);
-		return "home";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv;
 	}
 
 }
